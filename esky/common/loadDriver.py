3fa120332920c296f61e859e88ebcee55316a033
Goldyn <robert@gociety.com>
2015-09-08 10:08:33 +0200

    asda

--- esky/common/loadDriver.py
+++ esky/common/loadDriver.py
@@ -14,7 +14,7 @@ class LoadDriver_realDevice(object):
         desired_caps['platformVersion'] = '4.3'
         desired_caps['deviceName'] = settings.emu
         # Since the app is already installed launching it using package and activity name       
-        desired_caps['app'] = r'F:\automaty\esky\com.esky.apk'
+        desired_caps['app'] = 'sauce-storage:com.esky.apk'
         # Adding appWait Activity since the activity name changes as the focus shifts to the ATP WTA app's first page
         #desired_caps['appWaitActivity'] = '.activity.root.TournamentList'
         return webdriver.Remote(settings.appiumHub2, desired_caps)
